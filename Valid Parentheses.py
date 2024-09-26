2e8bd4e7538d82352401a511f9cd17e06eb3c623
iremzeytinozu <iremzeytinozu@gmail.com>
2024-09-26 23:33:23 +0300

    Create Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        
+        close_map = {"(" : ")",
+                     "{" : "}",
+                     "[" : "]"
+                    }
+        opens = []
+
+        for symbol in s:
+
+            if symbol in close_map.keys():
+                opens.append(symbol)
+            
+            elif opens == [] or symbol != close_map[opens.pop()]:
+                return False
+
+        return opens == []
