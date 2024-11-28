341d01899853d2bddb5ae0a85bd417b43be2b4f4
iremzeytinozu <iremzeytinozu@gmail.com>
2024-11-28 23:43:29 +0300

    Create Group Anagram.py

---
+++ Group Anagram.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list)
+        for s in strs:
+            sortedS = ''.join(sorted(s))
+            res[sortedS].append(s)
+        return list(res.values())
