e179e478c40bb980503e303cbfb65a00fad979e6
iremzeytinozu <iremzeytinozu@gmail.com>
2024-11-29 23:38:53 +0300

    Create Top K Frequent Elements.py

---
+++ Top K Frequent Elements.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = {}
+
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            # Eğer num daha önce sözlükte varsa, onun mevcut değerini al. Yoksa 0 al.
+            count[num] = 1 + count.get(num,0)
+        
+        # count sözlüğündeki her bir sayı (num) ve bu sayının tekrar sayısını (cnt) alır.
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+
+        for i in range(len(freq)-1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
