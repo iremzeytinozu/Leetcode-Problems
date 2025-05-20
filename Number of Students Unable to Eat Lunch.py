dabe0a739bec8c32daeb0f1f22159de25f01a6fb
iremzeytinozu <iremzeytinozu@gmail.com>
2025-05-20 21:18:42 +0300

    Update Number of Students Unable to Eat Lunch.py

--- Number of Students Unable to Eat Lunch.py
+++ Number of Students Unable to Eat Lunch.py
@@ -1,18 +1,20 @@
 class Solution(object):
     def countStudents(self, students, sandwiches):
-        """
-        :type students: List[int]
-        :type sandwiches: List[int]
-        :rtype: int
-        """
-        res = len(students) # baslagictaki ogrenci sayisi
-        count = Counter(students) # ogrencilerin tercihlerini sayiyor (kac tane 1 kac tane 0)
+        res = len(students)
+
+        cnt = {}
+
+        for s in students:
+            if s not in cnt:
+                cnt[s] = 0
+            cnt[s] += 1
 
         for s in sandwiches:
-            if count[s] > 0:
+            if cnt[s] > 0:
                 res -= 1
-                count[s] -= 1
+                cnt[s] -= 1
             else:
                 return res
 
         return res
+        
