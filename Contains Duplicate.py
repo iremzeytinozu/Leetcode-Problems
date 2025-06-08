338ca3131f70b3569cd2a5f28ecfd7fd6673a044
iremzeytinozu <iremzeytinozu@gmail.com>
2025-06-08 15:01:28 +0300

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        if len(nums) == len(set(nums)):
-            return False 
+        
+        seen = set()
 
-        return True
+        for num in nums:
+            if num in seen:
+                return True
+                
+            seen.add(num)
+
+        return False
